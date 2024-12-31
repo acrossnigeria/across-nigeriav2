c014af098ea1df2206027f86260d7f59cc77bd32
acrossnigeria <entertainmentmethodz@gmail.com>
2024-12-31 08:54:40 -0800

    new update and bug fixes for giveaway quiz

--- src/pages/admin/gqrs.js
+++ src/pages/admin/gqrs.js
@@ -33,16 +33,16 @@ export default function Gqrs( { user }) {
     return (
         <div>
             <Container admin={user} page={'gqrs'}>
-                <div className="h-screen md:w-[100%] gap-3 border-1 flex flex-col pt-[250px] items-center">
+                {/* <div className="h-screen md:w-[100%] gap-3 border-1 flex flex-col pt-[250px] items-center">
                     <div className="flex flex-col gap-3 items-center">
                         <span>No winners selected</span>
                         <button onClick={getWinners} className={`bg-transparent flex flex-row border-1 border-gray-600 justify-center rounded-[5px] text-gray-800 hover:bg-gray-400 items-center w-[200px] h-[50px]`}>Generate winners</button>
                     </div>
-                </div>
-            {/* <div className="h-screen md:w-[100%] gap-3 border-1 flex flex-col pt-[250px] items-center">
+                </div> */}
+            <div className="h-screen md:w-[100%] gap-3 border-1 flex flex-col pt-[250px] items-center">
                 <UnderCIcon/>
                 <span>Page is still under construction</span>
-            </div> */}
+            </div>
             </Container> 
         </div>
     )
--- src/pages/api/admin/genGiveawayQuizWinners.js
+++ src/pages/api/admin/genGiveawayQuizWinners.js
@@ -1,19 +1,38 @@
-import { Elsie_Swash_Caps } from "next/font/google";
 import db from "../../../../utils/db";
 import Quiz from "@/models/Quiz";
 
 const Handler = async (req, res) => {
     if ( req.method==='GET' ) {
-        let counter = 0
-        await db.connect();
-        const plays = await Quiz.find();
-        plays.map( async (play) => {
-            play.correctAnswer = true;
-            await play.save();
-            counter++;
-            console.log(`play ${counter} modified and saved`);
-        })
-        console.log('plays:', plays);
+
+        const arr = [ 2, 7, 4, 1, 6 ];
+        const sorted = [];
+        for ( let i = 0; i<arr.length ; i++) {
+            let a = arr[i];
+            let b = arr[i+1];
+            console.log(a, b)
+        }
+
+
+        const playersAndPlays = {}
+        // await db.connect();
+        // const plays = await Quiz.find( { correctAnswer: true } ).populate( { path:'userId', select: 'name surname _id phone residence' });
+        // plays.map( (play) => {
+        //     let isCreated = Object.values(playersAndPlays).some( innerObj => innerObj.userId === play.userId._id); // checks if player already exists in playersAndPlays
+        //     if (isCreated) {
+        //         playersAndPlays[play.userId._id].plays = playersAndPlays[play.userId._id].plays+1  // adds the number of plays
+        //     } else {
+        //         //creates an object for the player and adds it to playersAndPlays object
+        //         playersAndPlays[play.userId._id] = {
+        //             fullname: `${play.userId.name} ${play.userId.surname}`,
+        //             email: play.email,
+        //             phone: play.userId.phone,
+        //             plays: 1,
+        //             residence: play.userId.residence,
+        //             userId: play.userId._id, 
+        //         }
+        //     }
+        // })
+        // console.log('plays:', playersAndPlays);
         res.status(200).json( { success: true  });
     } else {
         res.status(503).json( { error: 'Invalid method'} );
