0b9c6dd62307f4e9b292568bd1ac82ed45fdba6e
alimancs <aliman2952003@gmail.com>
2025-01-22 01:27:06 +0100

    ambassador ui touch

--- src/pages/ambassador/apply.js
+++ src/pages/ambassador/apply.js
@@ -22,10 +22,11 @@ const Reg = () => {
                     <div className='h-[100px] mt-[100px] md:w-[50%] w-full absolute bg-gradient-to-b from-transparent to-white'></div>
                 </div>
                 <span className='text-[20px] text-green-500 px-[20px] font-extrabold text-center'>Join Our Ambassador Program &  Make a Difference!</span>
-                <form className='w-full flex flex-col mt-[20px]'>
+                <span className='text-[19px] font-light italic mt-[15px] self-start ml-[20px]' >Fill the form bellow</span>
+                <form className='w-full text-[18px] flex flex-col border-t-1 pt-[20px]'>
                     <div className='flex flex-col  gap-1 w-[100%] px-[15px]'>
                         <label htmlFor='status'>Current status</label>
-                        <select id='status' className='h-[48px] bg-gray-100 px-[10px] rounded-[5px] w-[100%]' >
+                        <select onChange={(e)=>{setCurrentStatus}} id='status' className='h-[48px] bg-gray-200 px-[10px] rounded-[5px] w-[100%]' >
                             <option className='focus:text-gray-200 hover:text-gray-400' value={'student'}> Student</option>
                             <option className='focus:text-gray-200 hover:text-gray-400' value={'corper'}>Youth service corper (NYSC)</option>
                             <option className='focus:text-gray-200 hover:text-gray-400' value={'Other'}>Other</option>
@@ -33,11 +34,11 @@ const Reg = () => {
                     </div>
                     <div className='flex flex-col gap-1 w-[100%] mt-[25px] px-[15px]'>
                         <label htmlFor='location'>Please write your University name/ NYSC camp center/ Work place</label>
-                        <input type='text' id='location' placeholder='Enter your university name/ NYSC camp/ Work place' className='h-[48px] bg-gray-100 px-[10px] rounded-[5px] w-[100%]' />
+                        <input type='text' id='location' placeholder='Enter your university name/ NYSC camp/ Work place' className='h-[48px] bg-gray-200 px-[10px] rounded-[5px] w-[100%]' />
                     </div>
                     <div className='flex flex-col gap-1 w-[100%] mt-[25px] px-[15px]'>
                         <label htmlFor='why'>Why do you want to become an Ambassador to represent Across Nigeria Reality Show?</label>
-                        <textarea id='why' placeholder='Write here...' className='h-[90px] bg-gray-100 px-[10px] rounded-[5px] w-[100%]'></textarea>
+                        <textarea id='why' placeholder='Write here...' className='h-[90px] bg-gray-200 px-[10px] rounded-[5px] w-[100%]'></textarea>
                     </div>
                     <div className='flex flex-col gap-1 w-[100%] mt-[25px] px-[15px]'>
                         <label >Are you willing to attend events/meetings on call or in person?</label>
@@ -48,17 +49,17 @@ const Reg = () => {
                     </div>
                     <div className='flex flex-col gap-1 w-[100%] mt-[25px] px-[15px]'>
                         <label htmlFor='state'>Which state do you currently reside in?</label>
-                        <select id='state' className='h-[48px] bg-gray-100 px-[10px] rounded-[5px] w-[100%]' >
+                        <select id='state' className='h-[48px] bg-gray-200 px-[10px] rounded-[5px] w-[100%]' >
                             <option disabled>Select state</option>
                             { nigeriaStates.map(state=> { return <option className='focus:text-gray-200 hover:text-gray-400' key={state} value={state}>{state}</option>} )}
                         </select>
                     </div>
                     <div className='flex flex-col gap-1 w-[100%] mt-[25px] px-[15px]'>
                         <label htmlFor='city'>Please write the name of the city/ town/ village where you reside</label>
-                        <input type='text' id='city' placeholder='Enter city/ town/ village' className='h-[48px] bg-gray-100 px-[10px] rounded-[5px] w-[100%]' />
+                        <input type='text' id='city' placeholder='Enter city/ town/ village' className='h-[48px] bg-gray-200 px-[10px] rounded-[5px] w-[100%]' />
                     </div>
                     <div className='px-[15px] mt-[30px] flex flex-row items-top gap-2'>
-                        <input className='h-[20px] w-[20px]' value={termsAgree} onChange={(e)=>{setTermsAgree(e.target.value)}} checked={termsAgree} type='checkbox'/>
+                        <input className='h-[30px] w-[30px]' value={termsAgree} onChange={(e)=>{setTermsAgree(e.target.value)}} checked={termsAgree} type='checkbox'/>
                         <span className='text-green-600'>I certify that the information provided is accurate and agree to the terms of the ambassador program</span>
                     </div>
                     <button className='h-[43px] w-[90%] self-center bg-green-500 hover:bg-green-600 rounded-[30px] text-white mt-[30px]' type='submit'>Apply</button>
