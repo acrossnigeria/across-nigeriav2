cc8dde99032ff3ad43ab30c205721234bf85602b
alimancs <aliman2952003@gmail.com>
2025-02-10 10:54:31 -0800

    adjustment  to UI text for login page

--- src/pages/account/login.js
+++ src/pages/account/login.js
@@ -11,13 +11,13 @@ import EyeOpen from "../../../public/images/icon/EyeOpen";
 import EyeClose from "../../../public/images/icon/EyeClose";
 
 export default function LoginScreen() {
-  const[loading,setLoading]=useState(false);
+  const [ loading,setLoading ] = useState(false);
   const { data: session } = useSession();
 
   const router = useRouter();
   const { redirect } = router.query;
-  const sessionName=session?.user?? null
-  const [showPassword, setShowPassword] = useState(false);
+  const sessionName = session?.user?? null;
+  const [ showPassword, setShowPassword ] = useState(false);
 
   const togglePasswordVisibility1 = () => {
     setShowPassword(!showPassword);
@@ -64,17 +64,15 @@ export default function LoginScreen() {
   return (
     <div>
       <Loader/>
-      <div className='flex flex-row justify-end px-8 py-3'>
+      <div className='flex flex-row justify-end bg-gray-100 px-8 py-3'>
           <Link href={'/'}><Close/></Link> 
       </div>
-      <div className="h-screen w-screen pt-11 mx-auto px-10 text-black ">
-        <div className='border-b-1 border-green-100 py-3 text-center text-[17px] font-bold text-green-600'>
-          <span>ACROSS NIGERIA REALITY SHOW</span>
-        </div>
-        <form className="mt-10 mx-auto rounded-md md:max-w-[500px]" onSubmit={handleSubmit(submitHandler)}>
-          <h1 className="mb-8 font-bold text-xl">Login</h1>
+      <div className="h-screen w-screen pt-11 mx-auto bg-gray-100 text-black ">
+        <form className="mt-3 mx-auto w-[93%] md:max-w-[500px] flex flex-col" onSubmit={handleSubmit(submitHandler)}>
+          <span className="mb-1 text-[25px]">Login</span>
+          <span className="mb-10">Welcome back.</span>
           <div className="mb-4">
-            <label htmlFor="email" className="font-semibold text-sm">Email</label>
+            <label htmlFor="email" className="text-[17px] ml-2 mb-3">Email</label>
             <input
               type="email"
               placeholder="Example@gmail.com"
@@ -85,7 +83,7 @@ export default function LoginScreen() {
                   message: 'Please enter valid email',
                 },
               })}
-              className="w-full text-[19px] focus:outline-gray-600 h-[49px] px-2 rounded-md bg-gray-200 text-black"
+              className="w-full border-gray-400 border-1 h-[45px] px-3 outline-none rounded-[15px] bg-gray-200"
               id="email"
               autoFocus
             ></input> 
@@ -94,18 +92,18 @@ export default function LoginScreen() {
             )}
           </div>
           <div className="mb-6 ">
-            <label htmlFor="password" className=" text-[17px] mb-[5px] ">  Password</label>
-            <div className="bg-gray-200 h-full flex flex-row rounded-[5px]">
+            <label htmlFor="password" className=" text-[17px] mb-3 ml-2 ">  Password</label>
+            <div className="h-[45px] flex flex-row border-gray-400 px-3 border-1 rounded-[15px] bg-gray-200">
               <input
                 type={showPassword?'text':'password'}
                 {...register('password', {
                   required: 'Please enter password',
                   minLength: { value: 6, message: 'password is more than 5 chars' },
                 })}
-                className="w-full h-[49px] focus:outline-none px-2 rounded-md bg-gray-200 text-black"
+                className="w-full bg-gray-200 outline-none"
                 id="password"
               ></input>
-              <button type="button" style={{borderRadius:'5px', height:'49px'}}
+              <button type="button" style={{borderRadius:'5px', height:'45px'}}
                 className={`right-0 px-[5px] w-fit` } onClick={togglePasswordVisibility1} >
                   {showPassword? <EyeOpen/>: <EyeClose/>}
               </button>
@@ -114,11 +112,13 @@ export default function LoginScreen() {
               <div className="text-red-500 ">{errors.password.message}</div>
             )}
           </div>
-          <div className="flex flex-row justify-between">
-          <button className="text-[16px] rounded-[5px] h-[49px] hover:bg-green-700 hover:border-none hover:text-white text-green-700 border-1 border-green-700 w-[48%]"><Link href="/user/passreset" className="mt-3">Forgot Password?</Link></button>
-          {loading?(<button disabled className="text-slate-100 h-[49px] animate-pulse w-[48%] text-[19px] py-1 rounded-sm bg-yellow-500">Please Wait...</button>):(<button className="text-white w-[48%] h-[49px] rounded-[5px] bg-green-700 hover:bg-green-900 active:bg-green-950">Login</button>)} 
+          <div className="flex flex-row justify-end">
+            <button className="text-[17px] hover:text-green-900 hover:scale-105"><Link href="/user/passreset" className="mt-2 mr-2">Forgot Password?</Link></button>
+          </div>
+          <div className="mt-2">
+            {loading?(<button disabled className="text-slate-100 h-[49px] animate-pulse w-[100%] text-[19px] py-1 rounded-[30px] bg-gray-500">Please Wait...</button>):(<button className="text-white w-[100%] h-[49px] rounded-[30px] bg-green-700 hover:bg-green-900 active:bg-green-950">Log In</button>)} 
           </div> 
-          <div className="mb-4 mt-6 text-[16px] font-semibold ">
+          <div className="mb-4 mt-6 text-[17px] ">
             Don&apos;t have an account? &nbsp;
             <Link className="text-green-700 cursor-pointer underline hover:text-green-400"  href={`/account/reg?redirect=${redirect || '/'}`}>Register</Link>
           </div>
