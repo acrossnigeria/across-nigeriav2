2369abfdc47d2e1f78b3a66765922dd18804400a
alimancs <aliman2952003@gmail.com>
2025-02-10 06:01:27 -0800

    added functionality to quiz show reg form for testing:retouch

--- src/components/ExitConfirmScreen.js
+++ src/components/ExitConfirmScreen.js
@@ -7,8 +7,8 @@ export default function ExitConfirmScreen({ to, cancelFunction }) {
             <div className="flex flex-col md:py-[30px] py-[50px] md:w-fit w-[90%] justify-center items-center rounded-[10px] bg-gray-100 px-[30px] ">
                 <span className="font-bold">Are you sure you want to exit?</span>
                 <div className="flex mt-[20px] flex-row gap-4">
-                    <button onClick={()=>{router.push(to)}} className="text-black bg-gray-300 rounded-full w-40">Yes</button>
-                    <button onClick={()=>{cancelFunction()}} className="bg-green-500 w-40 text-white rounded-[30px] px-[10px] py-[10px]">No</button>
+                    <button onClick={()=>{router.push(to)}} className="text-black bg-gray-300 rounded-full w-32">Yes</button>
+                    <button onClick={()=>{cancelFunction()}} className="bg-green-500 w-32 text-white rounded-[30px] px-[10px] py-[10px]">No</button>
                 </div>
             </div>
         </div>
