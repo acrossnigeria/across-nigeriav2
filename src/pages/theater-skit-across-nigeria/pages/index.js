2eaefddff33791b92bb7a18314e7bad3d9860de6
alimancs <aliman2952003@gmail.com>
2025-03-19 11:18:59 -0700

    additional fixes for skit competition index page

--- src/pages/theater-skit-across-nigeria/pages/index.js
+++ src/pages/theater-skit-across-nigeria/pages/index.js
@@ -12,6 +12,8 @@ import AddIcon from '../../../../public/images/icon/AddIcon';
 import HomeIcon from '../../../../public/images/icon/HomeIcon';
 import ProfileIcon from '../../../../public/images/icon/ProfileIcon';
 import { useSession } from 'next-auth/react';
+import logo1 from "../../../../public/images/logo1.png";
+import Close from "../../../../public/images/icon/Close";
 
 
 function SkitsPage() {
@@ -33,6 +35,10 @@ function SkitsPage() {
   const [ isGettingSkits, setIsGettingSkits ] = useState(true);
   const [ errorGettingSkit, setErrorGettingSkit ] = useState(false);
   const [ errorMessage, setErrorMessage ] = useState('');
+
+  const [ nlModalOpacity, setNlModalOpacity ] = useState('opacity-0');
+  const [ nlBgOpacity, setNlBgOpacity ] = useState('opacity-0');
+  const [ showModal, setShowModal ] = useState(false);
   
   function bubbleSort(arr) {
     for (let i = 0; i < arr.length - 1; i++) {
@@ -118,10 +124,58 @@ function SkitsPage() {
     getAllSkits();
   }
 
+  const toUploadPage = () => {
+    if (session?.user?.name) {
+        router.push('/theater-skit-across-nigeria/pages/add-skit');
+    } else {
+        notLoggedIn('in');
+    }
+}
+
+const notLoggedIn = (transiton) => {
+    if (transiton==='in') {
+        setShowModal(true);
+        setTimeout(() => {
+            setNlBgOpacity('opacity-100');
+            setNlModalOpacity('opacity-100');
+        }, 300);
+    } else {
+        setNlBgOpacity('opacity-0');
+        setNlModalOpacity('opacity-0');
+        setTimeout(() => {
+            setShowModal(false);
+        }, 1000);
+    }
+}
+
   return (
     <Layout hideNav={session?.user?.name?true:false}>
       <div className='md:ml-[5%] md:mr-[5%] bg-gray-100 mt-3'>
 
+        { showModal && 
+            <div className={`fixed ${nlBgOpacity} transition-all duration-300 ease-in-out backdrop-blur-sm h-screen w-screen flex flex-co items-center justify-center gap-3 bg-black/50 z-[1000] top-0`}>
+                <div className="h-fit flex flex-col justify-center w-[100%] items-center">
+                    <button onClick={()=>{notLoggedIn('out')}} className="border-1 text-[14px] flex flex-row gap-2 text-white hover:bg-green-600/50 hover:scale-105 transition-all duration-300 ease-in-out justify-center items-center px-[20px] py-1 rounded-[20px] mb-[20px] border-gray-100">
+                        <Close bg={'white'} size={'15px'}/>
+                        Close
+                    </button>
+                    <div className={`overflow-hidden h-[400px] md:w-[400px] transition-all duration-500 ease-in-out w-[80%] text-center ${nlModalOpacity} p-3 flex flex-col justify-center items-center bg-gray-100 rounded-[5px]`}>
+                        <div className='text-center mb-[35px] flex flex-row justify-center gap-1 items-center'>
+                            <Image src={logo1} alt='logo' placeholder='blur' className='h-[30px] w-[35px]' />
+                            <div className='flex flex-col justify-center items-start gap-0'>
+                                <span className='text-[12px] font-semibold text-green-700'>ACROSS NIGERIA</span>
+                                <span className='text-[10px] text-green-500'>REALITY SHOW</span>
+                            </div>
+                        </div>
+                        <span className="text-[14px]">Oops! You have to Log In or Register</span>
+                        <div className="flex mt-[20px] flex-col items-center gap-2">
+                            <button onClick={()=>router.push('/account/reg')} className="h-[40px] px-[20px] text-white bg-green-500 hover:bg-green-700 rounded-[20px]">Register Now</button>
+                            <button onClick={()=>router.push('/account/login')} className="text-gray-600 hover:text-black">Log In</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        }
         { errorGettingSkit && 
           <div className='h-screen w-full pt-[30px] flex flex-col gap-10'>
             <div className='text-red-500 font-light text-center md:w-[30%] w-[90%] mx-auto text-[13px]'>{errorMessage}. please check your internet connection</div>
@@ -201,7 +255,7 @@ function SkitsPage() {
                     <div className='mx-w-[70%] text-[14px] text-center mt-[15px]'>⏳ The stage is set! the competition starts now.</div>
                     <div className='mx-w-[70%] text-[14px] text-center'>🚀 Be the first to showcase your talent! Upload your skit now and compete for amazing cash prizes on Across Nigeria Reality Show! 🏆 Don&apos;t miss this chance to shine!</div>
                     <div className='flex md:flex-row flex-col mt-[10px] gap-2 justify-between items-center'>
-                      <button onClick={()=>{router.push('/theater-skit-across-nigeria/pages/add-skit')}} className='h-[35px] w-[130px] text-[15px] bg-green-600 hover:bg-green-700 text-white rounded-[25px]'>Upload Your Skit</button>
+                      <button onClick={toUploadPage} className='h-[35px] w-[130px] text-[15px] bg-green-600 hover:bg-green-700 text-white rounded-[25px]'>Upload Your Skit</button>
                       <button onClick={()=>{router.push('/theater-skit-across-nigeria/pages/landing')}} className='h-[35px] w-[130px] text-[15px] border-1 border-black bg-transparent hover:bg-gray-300 rounded-[25px]'>Learn more</button>
                     </div>
                   </div>
