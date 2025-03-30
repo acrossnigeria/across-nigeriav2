1d5d7a5b1234f244dd1f86d9d05c5ad19bbae76e
alimancs <aliman2952003@gmail.com>
2025-03-29 19:15:24 -0700

    adjustment for skit competition banners:ADJUSTMENT

--- src/components/SkitCompetitionClickCard.js
+++ src/components/SkitCompetitionClickCard.js
@@ -9,7 +9,7 @@ const SkitCompetitionClickCard = () => {
             <span className="bg-clip-text text-center text-transparent text-[20px] font-bold bg-gradient-to-b from-green-400 to-green-600">🏆 Join the Ultimate Skit Challenge & Win Big! 🎭</span>
             <div className="font-semibold">🎬 Are You Ready to Shine?</div>
             <div className="md:w-[70%] w-[90%] text-center">Think you&apos;ve got what it takes to create the funniest, most creative skit? Across Nigeria Reality Show is giving you the chance to showcase your talent and win up to &#8358;100,000</div>
-            <span className="text-[14px] text-center text-red-500">Competitions ends on the april 31st</span>
+            <span className="text-[14px] text-center text-red-500">Competitions ends on April 30st</span>
             <button onClick={()=>{router.push('/theater-skit-across-nigeria/pages/landing')}} className="px-[30px] h-[40px] mt-[10px] text-white bg-green-500 hover:bg-green-600 hover:scale-105 transition-all ease-in-out duration-250 rounded-[25px]">Enter The Competition</button>
         </div>
     )
