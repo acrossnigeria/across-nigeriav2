5696ce8fe2b14638b1affa379d5568d6fd632a5a
alimancs <aliman2952003@gmail.com>
2025-04-02 02:56:18 -0700

    adjust to skit challenge home page card 2

--- src/components/SkitCompetitionClickCard.js
+++ src/components/SkitCompetitionClickCard.js
@@ -6,7 +6,7 @@ const SkitCompetitionClickCard = () => {
 
     return (
         <div className="flex flex-col md:w-[100%] w-[94%] mx-auto md:h-[300px] h-[390px] border-[0.5px] border-green-600 p-3 rounded-[8px] justify-center items-center bg-gradient-to-b from-white to-gray-200 gap-2">
-            <span className="bg-clip-text text-center text-transparent text-[20px] font-extrabold bg-gradient-to-b from-red-500 to-yellow-400">-ACROSS NIGERIA SKIT CHALLENGE-</span>
+            <span className="bg-clip-text text-center text-transparent text-[20px] font-extrabold bg-gradient-to-b from-red-500 to-yellow-400">ACROSS NIGERIA SKIT CHALLENGE</span>
             <div className="text-center text-[17px] md:w-[60%] w-[80%] font-bold">🏆 Join the Ultimate Skit Challenge & Win Big! 🎭. 🎬 Are You Ready to Shine?</div>
             <div className="md:w-[70%] w-[90%] text-center">Think you&apos;ve got what it takes to create the funniest, most creative skit? Across Nigeria Reality Show is giving you the chance to showcase your talent and win up to &#8358;100,000</div>
             <span className="text-[14px] text-center text-red-500">Competitions ends on April 30th</span>
