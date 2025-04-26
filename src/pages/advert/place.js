fe19fd04023f9c7fb55eeaa2fe1fdac5cc696068
alimancs <aliman2952003@gmail.com>
2025-04-26 12:28:58 -0700

    select advert type screen

--- src/pages/advert/place.js
+++ src/pages/advert/place.js
@@ -12,14 +12,22 @@ const Page = () => {
     const router = useRouter();
     const { data:session } = useSession();
     const [ step, setStep ] = useState(1);
+    
+    const [ advertType, setAdvertType ] = useState(0);
+
+    const nextScreen = () => {
+        if (step<4) {
+            setStep(step+1)
+        }
+    }
 
 
     return (
-        <div className="pt-[5%] bg-green-800 flex flex-col h-screen">
+        <div className="pt-[5%] bg-gray-200 flex flex-col h-screen">
             <div className="pl-[3%] text-[18px] w-[100%]">
-                <button onClick={()=>{router.push('/')}} className="w-fit flex flex-row items-center transition-all duration-500 ease-in-out hover:scale-105 text-white gap-2"><div className="rotate-180"><Next bg={'black'} size={'20px'}/></div>Go back</button>
+                <button onClick={()=>{router.push('/')}} className="w-fit flex flex-row items-center transition-all duration-500 ease-in-out hover:scale-105 gap-2"><div className="rotate-180"><Next bg={'black'} size={'20px'}/></div>Go back</button>
             </div>
-            { step === 1 && <StepOne/>}
+            { step === 1 && <StepOne nextScreen={nextScreen} selectAdvert={setAdvertType} selectedAdvert={advertType}/>}
         
         </div>
     )
